fc974ba5e811ea177667dba54e12b90d543d8ac3
Sharan <skbhatpalla@gmail.com>
2017-05-27 22:43:55 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(error) => {
+        console.log('Unable to log to file server.log');
+    });
+    next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screemIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+    // res.send('Hello Express');
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name:'Sharan',
+    //     likes:['Biking','Travel']
+    // });
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMsg:'Welcome Page Here'
+    })
+});
+
+app.get('/about',(req,res) => {
+    // res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle:'About Page'
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage:'Unable to precess your request'
+    });
+});
+
+// app.listen(3000);
+app.listen(3000,() => {
+    console.log('Server is up and running on port 3000');
+});
