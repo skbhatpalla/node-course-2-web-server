6e63bdb742a623cd875c71d8f3593781324eb83b
Sharan <skbhatpalla@gmail.com>
2017-05-28 11:45:37 +0530

    Add project page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about',(req,res) => {
     });
 });
 
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs',{
+        pageTitle:'Projects Page',
+        projectMsg:'Please check our Projects'
+    });
+});
+
 app.get('/bad', (req,res) => {
     res.send({
         errorMessage:'Unable to precess your request'
